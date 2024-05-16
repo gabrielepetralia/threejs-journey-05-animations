05e2a34f696149b7422914d0fb48c498bc9c73d9
Gabriele <gabri.petralia03@gmail.com>
2024-05-16 23:01:43 +0200

    clock

--- src/script.js
+++ src/script.js
@@ -30,19 +30,31 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 
 // Time
-let time = Date.now()
+// let time = Date.now()
+
+// Clock
+const clock = new THREE.Clock()
 
 // Animations
 const tick = () => {
 
     // Time
-    const currentTime = Date.now()
-    const deltaTime = currentTime - time
-    time = currentTime
+    // const currentTime = Date.now()
+    // const deltaTime = currentTime - time
+    // time = currentTime
 
+    // Clock
+    const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y += 0.001 * deltaTime
+    // mesh.rotation.y += 0.001 * deltaTime
+    // mesh.rotation.y = elapsedTime * Math.PI * 2
+    // mesh.position.y = Math.sin(elapsedTime)
+    // mesh.position.x = Math.cos(elapsedTime)
+
+    camera.position.y = Math.sin(elapsedTime)
+    camera.position.x = Math.cos(elapsedTime)
+    camera.lookAt(mesh.position)
 
     // Render
     renderer.render(scene, camera)
